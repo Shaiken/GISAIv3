1e67e75d5b0e63343758ad0ab0c3a30b5d2827d9
shaiken <shaiken@gis.tw>
2022-08-23 11:07:08 +0800

    first test

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import {
+  showDialog,
+  Dialog,
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  JupyterFrontEnd, JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import {
+  IMainMenu
+} from '@jupyterlab/mainmenu';
+
+import {
+  Widget
+} from '@lumino/widgets';
+
+import '../style/index.css';
+
+namespace CommandIDs {
+  export const shutdown = 'hub:shutdown';
+}
+
+
+/**
+ * Initialization data for the jupyterlab-ext-changeconf extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'GISAIv3_1',
+  autoStart: true,
+  requires: [
+    ICommandPalette,
+    IMainMenu
+  ],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette, mainMenu: IMainMenu) => {
+    app.commands.addCommand(CommandIDs.shutdown, {
+      label: 'Shutdown Current Session',
+      execute: () => {
+        let widget: Widget = new Widget();
+	widget.id = 'End Notebook Session';
+	widget.node.innerHTML = "All running notebooks will be stopped.<br/><br/>Do you wish to continue?";
+        showDialog({
+          title: 'End Notebook Session',
+	  body: widget,
+          focusNodeSelector: 'input',
+	  buttons: [
+	    Dialog.cancelButton({ label: 'No' }),
+	    Dialog.warnButton({ label: 'Yes' })
+	  ]
+        }).then(result => {
+          if (result.button.label === 'Yes') {  
+            window.location.replace('/hub/home?changeconfig');
+	  }
+	});
+      }
+    });
+
+    mainMenu.fileMenu.addGroup([{ command: CommandIDs.shutdown }], 100);
+  }
+};
+
+export default extension;
